05d0346406813eded3238df7014fedd216a6e967
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-05-29 10:21:53 +0530

    Time: 33 ms (31.71%), Space: 45.2 MB (6.37%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -15,11 +15,26 @@ class Solution {
             return dp[n][m]= max(solve(s,k,n-1,m,dp),solve(s,k,n,m-1,dp));
         }
     }
+    int solveTab(string &s, string &k){
+        vector<vector<int>>dp(s.size()+1,vector<int>(k.size()+1,0));
+
+        for(int i = 1 ;i<=s.size() ; i++){
+            for(int j = 1; j<=k.size() ;j++){
+                if(s[i-1]==k[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[s.size()][k.size()];
+    }
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
         int m = text2.size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        return solve(text1,text2,text1.size()-1,text2.size()-1,dp);
+        return solveTab(text1,text2);
+        // return solve(text1,text2,text1.size()-1,text2.size()-1,dp);
     }
 };
