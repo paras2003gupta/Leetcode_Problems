3e396a855243283693f8abfca6d918013e8515b0
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-05-31 16:31:15 +0530

    Time: 86 ms (43.97%), Space: 296.4 MB (20.14%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,3 +1,5 @@
+//this code includes printing of the Longest Increasing subsequence also
+
 class Solution {
     private:
     int solve(vector<int>&nums,int i,int prev,vector<vector<int>>&dp){
@@ -46,20 +48,58 @@ class Solution {
     }
     int method2(vector<int>&nums){
         vector<int>dp(nums.size(),1);
+        vector<int>hash(nums.size(),0);
+        for(int i = 0 ; i<nums.size() ; i++){
+            hash[i]=i;
+        }
+
         int maxi = 1;
         for(int i = 0; i<nums.size() ; i++){
             for(int prev = 0 ; prev<i ; prev++){
                 if(nums[prev]<nums[i]){
+                    hash[i] = prev;
                     dp[i] = max(dp[i],1+dp[prev]);
                     maxi = max(maxi,dp[i]);
                 }
             }
         }
+        int start;
+        for(int i = 0;i<nums.size() ; i++){
+            if(dp[i]==maxi){
+                start = i;
+            }
+        }
+        vector<int>ans;
+        while(hash[start]!=start){
+            ans.push_back(nums[start]);
+            start = hash[start];
+        }
+        ans.push_back(nums[start]);
+        reverse(ans.begin(),ans.end());
+        cout<<"One of the Longest Increasing solution is ->";
+        for(auto it:ans){
+            cout<<it<<" ";
+        }cout<<endl;
         return maxi;
+    }
+    int method3BS(vector<int>&nums){
+            vector<int>temp;
+            temp.push_back(nums[0]);
+
+            for(int i = 1; i<nums.size() ; i++){
+                if(nums[i]>temp.back()){
+                    temp.push_back(nums[i]);
+                }else{
+                    int ind = lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                    temp[ind] = nums[i];
+                }
+            }
+            return temp.size();
     }
 public:
     int lengthOfLIS(vector<int>& nums) {
         vector<vector<int>>dp(nums.size(),vector<int>(nums.size()+1,-1));
+        return method3BS(nums);
         return method2(nums);
         return solveTab(nums);
         return solve(nums,0,-1,dp);
