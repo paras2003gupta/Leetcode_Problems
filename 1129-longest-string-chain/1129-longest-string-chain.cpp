697f0af8b4d8f546477f31dfb36b1b669c74cab9
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-05-31 18:14:53 +0530

    Time: 1700 ms (23.72%), Space: 638.6 MB (22.95%) - LeetHub

---
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+bool checkOneLetterDiff(string a , string b){
+    if(a.size()-1!=b.size()) return false;
+    int second = 0;
+    int first = 0;
+    while(first<a.size()){
+       if(a[first]==b[second]){
+        first++;second++;
+       }else{
+        first++;
+       }
+    }
+    return first==a.size()&&second==b.size();
+}
+static bool cmp(string &a, string &b){
+    return a.size()<b.size();
+}
+public:
+    int longestStrChain(vector<string>& words) {
+        sort(words.begin(),words.end(),cmp);
+        int n = words.size();
+        vector<int>dp(n,1);
+        int maxi= INT_MIN;
+        for(int i = 0 ; i< n ; i++){
+           
+            for(int prev = 0 ; prev<i ; prev++){
+                if(checkOneLetterDiff(words[i],words[prev])&&dp[i]<dp[prev]+1){
+                    dp[i] = dp[prev]+1;
+                }
+            }
+            maxi = max(maxi,dp[i]);
+
+        }
+        
+        return maxi;
+    }
+};
