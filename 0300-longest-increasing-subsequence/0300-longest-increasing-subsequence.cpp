4df2e9d8f9e523f39e63c564cd6d617251ac31e0
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-05-31 15:15:30 +0530

    Time: 378 ms (25.83%), Space: 576.9 MB (5.93%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -13,9 +13,25 @@ class Solution {
         }
         return dp[i][prev+1]= len;
     }
+    int solveTab(vector<int>&nums){
+        int n = nums.size();
+        vector<vector<int>>dp(nums.size()+2,vector<int>(nums.size()+10,0));
+
+        for(int i = n-1 ; i>=0 ; i--){
+            for(int prev = i-1 ; prev>=-1 ; prev--){
+                int len = dp[i+1][prev+1];
+                if(prev==-1||nums[i]>nums[prev]){
+                    len = max(len,1+dp[i+1][i+1]);
+                }
+                dp[i][prev+1] = len;
+            }
+        }
+        return dp[0][0];
+    }
 public:
     int lengthOfLIS(vector<int>& nums) {
         vector<vector<int>>dp(nums.size(),vector<int>(nums.size()+1,-1));
+        return solveTab(nums);
         return solve(nums,0,-1,dp);
     }
 };
