17e2861c041be8a08edf99ba8351252caa3cfc34
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-06-03 16:30:18 +0530

    Time: 93 ms (78.32%), Space: 16.2 MB (7.24%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -11,11 +11,31 @@ public:
         }
         return dp[i][j]= maxi;
     }
+    int solveTab(vector<int>&nums){
+        int n = nums.size();
+        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+
+        for(int i = n-2;i>=1 ; i--){
+            for(int j = 1 ; j<=n-2 ; j++){
+                if(i>j)continue;
+                int maxi = INT_MIN;
+                for(int ind  = i ; ind<= j ; ind++ ){
+                    
+
+                    maxi = max(maxi,nums[i-1]*nums[ind]*nums[j+1]+dp[i][ind-1]+dp[ind+1][j]);
+                }
+
+                dp[i][j] = maxi;
+            }
+        }
+        return dp[1][n-2];
+    }
     int maxCoins(vector<int>& nums) {
         nums.insert(nums.begin(),1);
         nums.push_back(1);
         int n = nums.size();
         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return solveTab(nums);
         return solve(nums,1,n-2,dp);
 
     }
