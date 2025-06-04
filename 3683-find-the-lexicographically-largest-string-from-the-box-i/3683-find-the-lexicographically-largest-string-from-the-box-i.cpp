a66173d287354bb47b7ba9b2c5684ff77c8c29bf
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-06-04 13:17:58 +0530

    Time: 1 ms (97.01%), Space: 10.9 MB (87.69%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
+    string lastSubstring(string s) {
+        int i = 0, j = 1, n = s.size();
+        while (j < n) {
+            int k = 0;
+            while (j + k < n && s[i + k] == s[j + k]) {
+                k++;
+            }
+            if (j + k < n && s[i + k] < s[j + k]) {
+                int t = i;
+                i = j;
+                j = max(j + 1, t + k + 1);
+            } else {
+                j = j + k + 1;
+            }
+        }
+        return s.substr(i, n - i);
+    }
+
     string answerString(string word, int numFriends) {
         if (numFriends == 1) {
             return word;
         }
-        int n = word.size();
-        string res;
-        for (int i = 0; i < n; i++) {
-            res = max(res, word.substr(i, min(n - numFriends + 1, n - i)));
-        }
-        return res;
+        string last = lastSubstring(word);
+        int n = word.size(), m = last.size();
+        return last.substr(0, min(m, n - numFriends + 1));
     }
 };
