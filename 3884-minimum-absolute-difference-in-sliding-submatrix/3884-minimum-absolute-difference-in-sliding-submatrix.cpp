1ddfca03cb6b930e3242dc6548709ac666f94ef7
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-06-05 17:22:41 +0530

    Time: 10 ms (87.54%), Space: 32 MB (83.45%) - LeetHub

--- 3884-minimum-absolute-difference-in-sliding-submatrix/3884-minimum-absolute-difference-in-sliding-submatrix.cpp
+++ 3884-minimum-absolute-difference-in-sliding-submatrix/3884-minimum-absolute-difference-in-sliding-submatrix.cpp
@@ -9,27 +9,25 @@ public:
         }
         for(int i = 0; i<n-k+1 ;i++){
             for(int j = 0;j<m-k+1 ; j++){
-                set<int>st;
+                vector<int>ele ;
+                
+
                 for(int p = i ; p<min(i+k,n) ; p++){
                     for(int q = j ; q<min(j+k,m) ; q++){
-                        st.insert(grid[p][q]);
+                        ele.push_back(grid[p][q]);
                     }
                 }
-                vector<int>ele ;
-                for(auto it:st){
-                    ele.push_back(it);
-                }
+                
                 sort(ele.begin(),ele.end());
                 int mini = INT_MAX;
-                if(ele.size()==1){
-                    continue;
-                }
+                
 
                 for(int l = 0 ; l<ele.size()-1 ; l++){
                     int val = abs(ele[l]-ele[l+1]);
+                    if(val==0)continue;
                     mini = min(mini,val);
                 }
-                ans[i][j] = mini;
+                if(mini!=INT_MAX)ans[i][j] = mini;
 
 
 
