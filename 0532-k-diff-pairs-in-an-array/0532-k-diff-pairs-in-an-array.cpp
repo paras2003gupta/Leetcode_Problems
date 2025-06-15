b1c119d6aac11154e60b8d24520aa75c15aa9ced
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-06-15 15:10:50 +0530

    Time: 14 ms (25.98%), Space: 17.3 MB (28.33%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -2,24 +2,27 @@ class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
         unordered_map<int,int>mp;
-        set<pair<int,int>>st;
-        int cnt = 0;
-        for(int i = 0 ; i<nums.size() ; i++){
-            if(mp.find(nums[i]-k)!=mp.end()){
-                int a = max(nums[i]-k,nums[i]);
-                int b = min(nums[i]-k,nums[i]);
-                st.insert({a,b});
-                cout<<nums[i]-k<<" "<<nums[i]<<" "<<i<<endl;
-            }
-            if(mp.find(nums[i]+k)!=mp.end()){
-                int a = max(nums[i]+k,nums[i]);
-                int b = min(nums[i]+k,nums[i]);
-                st.insert({a,b});
-                cout<<k+nums[i]<<" "<<nums[i]<<" "<<i<<endl;
-            }
-
+        for(int i = 0;i<nums.size() ;i++){
             mp[nums[i]]++;
+        }  
+        int ans =0;
+        
+        if(k>0){
+            for(auto it:mp){
+                if(mp.count(it.first+k)>0)ans++;
+
+            }
+            return ans;
+
+        }else{
+            for(auto it:mp){
+                if(it.second>1){
+                    ans++;
+                }
+
+            }
+            return ans;
         }
-        return st.size();
+        return ans;
     }
 };
