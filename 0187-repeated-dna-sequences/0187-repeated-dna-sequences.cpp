63bfabc70b03528ab320da26ab94a58571d8422e
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-07-05 10:42:27 +0530

    Time: 590 ms (5.06%), Space: 24.7 MB (65.86%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        map<string,int>mp;
+        if(s.size()<=10){
+            return {};
+        }
+        string win = "";
+        for(int i =  0 ;i<10 ; i++){
+            win+=s[i];
+        }
+        mp[win]++;
+        for(int i = 10;i<s.size() ; i++){
+            win = win.substr(1);
+            win+=s[i];
+            mp[win]++;
+        }
+        vector<string>ans;
+        for(auto it:mp){
+            cout<<it.first<<" "<<it.second<<endl;
+        }
+        for(auto it:mp){
+            if(it.second>1){
+                ans.push_back(it.first);
+            }
+        }
+        return ans;
+    }
+};
