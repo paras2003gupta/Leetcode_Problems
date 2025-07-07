1161a81ab8153f7e28e30804916f27b3a2149b11
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-07-07 12:16:56 +0530

    Time: 55 ms (92.26%), Space: 74.8 MB (85.96%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
@@ -1,48 +1,29 @@
-const auto _ = std::cin.tie(nullptr)->sync_with_stdio(false);
-
-#define LC_HACK
-#ifdef LC_HACK
-const auto __ = []() {
-    struct ___ {
-        static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; }
-    };
-    std::atexit(&___::_);
-    return 0;
-}();
-#endif
-
-
 class Solution {
 public:
     int maxEvents(vector<vector<int>>& events) {
-    sort(events.begin(), events.end()); // sort by start day
-    priority_queue<int, vector<int>, greater<int>> pq; // min-heap on end day
-    
-    int n = events.size(), i = 0, res = 0;
-    int day = 1;
+        sort(events.begin(),events.end());
+        priority_queue<int,vector<int>,greater<int>>pq;
+        int n = events.size();
+        int day = events[0][0];
+        int i = 0;
+        int cnt = 0;//result number of events attended.
+        while(!pq.empty()||i<n){
+            while(i<n&&events[i][0]==day){
+                pq.push(events[i][1]);
+                
+                i++;
+            }
+            if(!pq.empty()){
+                pq.pop();
+                cnt++;//1 event is attended.
+            }
+            day++;
+            while(!pq.empty()&&pq.top()<day){
+                pq.pop();
+            }
+            
 
-    while (!pq.empty() || i < n) {
-        // Add all events that start today
-        while (i < n && events[i][0] == day) {
-            pq.push(events[i][1]);
-            i++;
         }
-
-        // Remove events that already ended
-        while (!pq.empty() && pq.top() < day) {
-            pq.pop();
-        }
-
-        // Attend event with earliest end day
-        if (!pq.empty()) {
-            pq.pop();
-            res++;
-        }
-
-        day++;
+        return cnt;
     }
-
-    return res;
-}
-
 };
