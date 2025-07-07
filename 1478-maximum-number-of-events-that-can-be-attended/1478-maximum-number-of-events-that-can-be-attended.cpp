3e482df35625ee59e4293413af34c1840920c8e4
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-07-07 12:18:01 +0530

    Time: 56 ms (87.51%), Space: 74.9 MB (64.96%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.cpp
@@ -1,29 +1,40 @@
 class Solution {
 public:
     int maxEvents(vector<vector<int>>& events) {
-        sort(events.begin(),events.end());
-        priority_queue<int,vector<int>,greater<int>>pq;
+        // Sabse pehle events ko unke start day ke basis pe sort karte hain
+        sort(events.begin(), events.end());
+
+        // Min-heap banate hain to store end days of events
+        priority_queue<int, vector<int>, greater<int>> pq;
+
         int n = events.size();
-        int day = events[0][0];
+        int day = events[0][0]; // pehla din jahan se events start ho rahe hain
         int i = 0;
-        int cnt = 0;//result number of events attended.
-        while(!pq.empty()||i<n){
-            while(i<n&&events[i][0]==day){
-                pq.push(events[i][1]);
-                
+        int cnt = 0; // final count of events jo attend kar paaye
+
+        // Jab tak koi event baaki hai ya heap mein koi event pending hai
+        while (!pq.empty() || i < n) {
+
+            // Jitne events current day se start ho rahe hain, unke end days heap mein daal do
+            while (i < n && events[i][0] == day) {
+                pq.push(events[i][1]); // event ka end day push karo
                 i++;
             }
-            if(!pq.empty()){
-                pq.pop();
-                cnt++;//1 event is attended.
-            }
-            day++;
-            while(!pq.empty()&&pq.top()<day){
-                pq.pop();
-            }
-            
 
+            // Agar koi event attend kar sakte ho, toh sabse pehle us event ko attend karo jiska end day sabse chhota hai
+            if (!pq.empty()) {
+                pq.pop(); // event attend ho gaya
+                cnt++;    // count badhao
+            }
+
+            day++; // agle din par chale jao
+
+            // Heap se saare expired events hata do (jin ka end day kal ke pehle tha)
+            while (!pq.empty() && pq.top() < day) {
+                pq.pop();
+            }
         }
-        return cnt;
+
+        return cnt; // total number of events attended
     }
 };
