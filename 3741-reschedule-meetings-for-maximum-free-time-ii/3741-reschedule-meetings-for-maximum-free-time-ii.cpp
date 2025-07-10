c29998e83210d81274c8f5f64ce2e29a95ff6be3
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-07-10 11:23:15 +0530

    Time: 52 ms (37.85%), Space: 148 MB (57.37%) - LeetHub

--- 3741-reschedule-meetings-for-maximum-free-time-ii/3741-reschedule-meetings-for-maximum-free-time-ii.cpp
+++ 3741-reschedule-meetings-for-maximum-free-time-ii/3741-reschedule-meetings-for-maximum-free-time-ii.cpp
@@ -1,38 +1,52 @@
 class Solution {
 public:
     int maxFreeTime(int eventTime, vector<int>& startTime, vector<int>& endTime) {
-        vector<int>gaps;
+        vector<int>gaps; // har do meeting ke beech ka gap store karne ke liye
         int n = startTime.size();
         int end = eventTime;
         int start = 0;
-        gaps.push_back(startTime[0]-start);
-        for(int i = 0 ; i<n-1 ; i++){
-            gaps.push_back(-endTime[i]+startTime[i+1]);
+
+        // Pehla gap: event ke start se pehli meeting tak ka
+        gaps.push_back(startTime[0] - start);
+
+        // Beech ke gaps: har 2 consecutive meetings ke beech ka
+        for(int i = 0 ; i < n - 1 ; i++){
+            gaps.push_back(startTime[i + 1] - endTime[i]);
         }
-        gaps.push_back(end-endTime[n-1]);
-        int maxi  =INT_MIN;
+
+        // Aakhri gap: last meeting ke baad se event end hone tak
+        gaps.push_back(end - endTime[n - 1]);
+
+        int maxi = INT_MIN;
+
+        // Copy of gaps banaya jise sort karenge later
         vector<int>sorted_gaps = gaps;
-        sort(sorted_gaps.begin(),sorted_gaps.end());
-        int i = 0;
-        for(; i<gaps.size()-1 ; i++){
-            int sum = gaps[i]+gaps[i+1];
-            
-                int toAdjust = endTime[i]-startTime[i];
-                int cnt = gaps.size()-(lower_bound(sorted_gaps.begin(),sorted_gaps.end(),toAdjust)-sorted_gaps.begin());
-                if(toAdjust<=gaps[i]){
-                    cnt--;
-                }
-                if(toAdjust<=gaps[i+1]){
-                    cnt--;
-                }
+        sort(sorted_gaps.begin(), sorted_gaps.end()); // O(n log n)
 
-                if(cnt>0){
-                    sum+=toAdjust;
-                }
+        // Har 2 consecutive gaps ke upar loop
+        for(int i = 0; i < gaps.size() - 1; i++){
+            int sum = gaps[i] + gaps[i + 1]; // current 2 gaps ka sum
 
-                maxi = max(maxi,sum);
-            
+            int toAdjust = endTime[i] - startTime[i]; // jis meeting ko shift karna hai uski duration
+            int cnt = gaps.size() - (lower_bound(sorted_gaps.begin(), sorted_gaps.end(), toAdjust) - sorted_gaps.begin());
+
+            // Agar current gap hi duration se bada hai toh count se hatao (kyunki wahi use ho gaya hoga)
+            if(toAdjust <= gaps[i]){
+                cnt--;
+            }
+            if(toAdjust <= gaps[i + 1]){
+                cnt--;
+            }
+
+            // Agar koi aur gap bacha hai jisme ye meeting aa sakti hai
+            if(cnt > 0){
+                sum += toAdjust; // duration ko add karo kyunki reschedule possible hai
+            }
+
+            // Maximum free time update karo
+            maxi = max(maxi, sum);
         }
+
         return maxi;
     }
 };
