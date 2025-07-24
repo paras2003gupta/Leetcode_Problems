f422804ab343c76653d675c8c85cd2d3d91e1fef
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-07-24 09:24:31 +0530

    Time: 0 ms (100%), Space: 10.8 MB (22.48%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,31 +1,20 @@
 class Solution {
-    private:
-    int memoisationrob(vector<int>nums,vector<int>&dp,int i){
-        if(i==0){
-            return dp[0]= nums[0];
-        }
-        if(i<0)return 0;
-
-        if(dp[i]!=-1)return dp[i];
-
-        int take = memoisationrob(nums,dp,i-2)+nums[i];
-        int nottake = memoisationrob(nums,dp,i-1);
-        
-        return  dp[i]=max(take,nottake);
-    }
 public:
-    int rob(vector<int>& nums) {
-        int prev = nums[0];
-        int prevprev = 0;
-        for(int i =1 ; i<nums.size() ; i++){
-            int take = nums[i]+prevprev;
-            int nottake = prev;
-            int curr = max(take,nottake);
-            prevprev = prev;
-            prev = curr;
-        }
-        vector<int>dp(nums.size()+1,-1);
-
-        return memoisationrob(nums,dp,nums.size()-1);
-    }
+    int rob(vector<int>& arr) {
+         int n = arr.size();
+            if(n==0){
+                return 0;
+            }
+            if(n==1){
+                return arr[0];
+            }
+            vector<int>dp(n+1,0);
+            dp[0] = 0;
+            dp[1] = arr[0];
+            
+            for (int i = 2; i <=n; i++) {
+                dp[i] = max(dp[i-1],arr[i-1]+dp[i-2]);
+            }
+            return dp[n];
+            }
 };
