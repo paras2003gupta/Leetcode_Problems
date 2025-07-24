08b1e570f6a3074599323d9b48f553da89029546
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-07-24 09:26:14 +0530

    Time: 0 ms (100%), Space: 10 MB (97.36%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -8,13 +8,14 @@ public:
             if(n==1){
                 return arr[0];
             }
-            vector<int>dp(n+1,0);
-            dp[0] = 0;
-            dp[1] = arr[0];
+            int prev0 = 0;
+            int prev1 = arr[0];
             
             for (int i = 2; i <=n; i++) {
-                dp[i] = max(dp[i-1],arr[i-1]+dp[i-2]);
+                int curr = max(prev1,arr[i-1]+prev0);
+                prev0=prev1;
+                prev1=curr;
             }
-            return dp[n];
+            return max(prev0,prev1) ;
             }
 };
