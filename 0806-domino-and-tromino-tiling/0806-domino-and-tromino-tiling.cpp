58ce9a8042abf4522a56c684064f51e0ffaf06db
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-09-08 09:39:51 +0530

    Time: 0 ms (100%), Space: 9.3 MB (17.92%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
@@ -1,15 +1,26 @@
+
 class Solution {
 public:
-    int mod = 1e9 + 7;
+    const int mod  = 1e9+7;
     int numTilings(int n) {
-        if (n <= 1) return 1;
-        if (n == 2) return 2;
-        if (n == 3) return 5;
-        vector<int> dp(n + 1, 0);
-        dp[0] = 1, dp[1] = 1, dp[2] = 2, dp[3] = 5;
-        for (int i = 4; i <= n; i++) 
-            dp[i] = (dp[i - 1] * 2 + long(dp[i - 3])) % mod;
+        vector<long long>dp(n+1,0);
+
+        dp[1] = 1;
+        if(n<=1){
+            return dp[n];
+        }
+        dp[2] = 2;
+        if(n<=2){
+            return dp[n];
+        }
+        dp[3] = 5;
+        if(n<=3){
+            return dp[n];
+        }
+        for(int i = 4; i<=n ; i++){
         
+            dp[i] = (2*dp[i-1]+dp[i-3])%mod;
+        }
         return dp[n];
     }
 };
