d0583178be0c0f56043b61368cd495e401641ebb
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-09-08 21:06:49 +0530

    Time: 49 ms (5.02%), Space: 111.1 MB (5.86%) - LeetHub

--- 1503-reducing-dishes/1503-reducing-dishes.cpp
+++ 1503-reducing-dishes/1503-reducing-dishes.cpp
@@ -14,10 +14,29 @@ public:
         ans = max(ans,solve(s,time,i+1,dp));
         return dp[i][time]= ans;
     }
+    int solveTab(vector<int>&s){
+        vector<vector<int>>dp(501,vector<int>(501,INT_MIN));
+        for(int i = 0 ; i<=s.size() ; i++){
+            dp[i][0] = 0;
+        }
+        dp[0][1] =s[0];
+        for(int i = 1;i<s.size() ; i++){
+            for(int time=  1; time<=s.size() ; time++){
+                long long include =  s[i]*time+1LL*dp[i-1][time-1];
+                long long exclude = dp[i-1][time];
+                dp[i][time] = max(include,exclude);
+            }
+        }
+        int ans= 0;
+        for(int i = 0 ; i<=s.size();i++){
+            ans = max(ans,dp[s.size()-1][i]);
+        }
+        return ans;
+    }
     int maxSatisfaction(vector<int>& s) {
         sort(s.begin(),s.end());
         vector<vector<int>>dp(s.size()+2,vector<int>(s.size()+2,-1));
-        int ans = solve(s,1,0,dp);
+        int ans = solveTab(s);
         return ans;
     }
 };
