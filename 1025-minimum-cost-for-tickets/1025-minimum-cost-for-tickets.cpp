c71bedb951a398da8ad4d1bdad8e285e4f712898
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-09-08 15:57:56 +0530

    Time: 0 ms (100%), Space: 13.2 MB (76.4%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-    int solve(vector<int>& days, vector<int>& costs, int days_cov, int i, vector<vector<int>>& dp){
-        if(i == days.size()) return 0;
-
-        if(dp[i][days_cov] != -1) return dp[i][days_cov];
-
+    int solve(vector<int>& days, vector<int>& costs,  int i, vector<int>& dp){
+        if(i >= days.size()) return 0;
+        if(dp[i]!=-1)return dp[i];
         int ans = INT_MAX;
-        if(days_cov >= days[i]){
-            ans = min(ans, solve(days, costs, days_cov, i+1, dp));
-        }
-        else{
+        
+    
             // 1-day pass
-            ans = min(ans, costs[0] + solve(days, costs, days[i], i+1, dp));
-            // 3-day pass
-            ans = min(ans, costs[1] + solve(days, costs, days[i]  +6, i+1, dp));
+            ans = min(ans, costs[0] + solve(days, costs, i+1, dp));
             // 7-day pass
-            ans = min(ans, costs[2] + solve(days, costs, days[i] + 29, i+1, dp));
-        }
-        return dp[i][days_cov] = ans;
+            int max_day = days[i]+6;
+            int j = i;
+            while(j<days.size()&&days[j]<=max_day){
+                j++;
+            }
+            ans = min(ans, costs[1] + solve(days, costs, j, dp));
+            // 30-day pass
+            j = i;
+            max_day = days[i]+29;
+            while(j<days.size()&&days[j]<=max_day){
+                j++;
+            }
+            ans = min(ans, costs[2] + solve(days, costs,  j, dp));
+        
+        return dp[i]= ans;
     }
 
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
-        vector<vector<int>> dp(n, vector<int>(400, -1)); 
+        vector<int> dp(n,-1); 
         // 400 is safe since max day is <= 365
-        return solve(days, costs, 0, 0, dp);
+        return solve(days, costs,  0, dp);
     }
 };
