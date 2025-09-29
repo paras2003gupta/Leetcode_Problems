f157efcf02f18d1016e6b1f27325ec9d8dc579de
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-09-29 16:01:39 +0530

    Time: 51 ms (17.33%), Space: 29.9 MB (19.6%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>>mp;
-        for(auto x : strs){
-            string word = x;
-            sort(word.begin(),word.end());
-            mp[word].push_back(x);
+        unordered_map<string, vector<string>> mp;
+        
+        for (auto& s : strs) {
+            // Create a frequency count key for 26 lowercase letters
+            int count[26] = {0};
+            for (char c : s)
+                count[c - 'a']++;
+            
+            // Build a unique string key from frequency count
+            string key;
+            for (int i = 0; i < 26; ++i) {
+                key += '#';
+                key += to_string(count[i]);
+            }
+            mp[key].push_back(s);
         }
-        vector<vector<string>>ans;
-        for(auto y : mp){
-            ans.push_back(y.second);
 
+        vector<vector<string>> ans;
+        for (auto& pair : mp)
 
-        }
+            ans.push_back(std::move(pair.second));
+
         return ans;
     }
 };
