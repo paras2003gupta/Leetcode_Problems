1a762f603401dc072439daa3b4b359c0fbd155b1
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-10-05 08:28:03 +0530

    Time: 63 ms (54.23%) | Memory: 21.2 MB (63.80%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         unordered_set<string>st(wordList.begin(),wordList.end());
-        if(st.find(endWord)==st.end()) return 0;
+        // if(st.find(endWord)==st.end()) return 0;
 
         queue<pair<string,int>>q;
         q.push({beginWord,1});
@@ -15,7 +15,7 @@ public:
             q.pop();
 
             if(word==endWord) return steps;
-            
+
             for(int i = 0 ;i<word.length();i++){
                 char original = word[i];
                 for(char k = 'a' ; k<='z' ; k++){
