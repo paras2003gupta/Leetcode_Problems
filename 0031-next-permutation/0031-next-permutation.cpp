2a4fed07ad9a3d9c42da26dff5310a825adce454
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-11-29 22:42:04 +0530

    Time: 0 ms (100%), Space: 15.7 MB (32.11%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        int i = n-2;
-        
-        while(i>=0&&nums[i]>=nums[i+1]){
-            i--;
+        int ind = -1;
+        for(int i = n-2;  i >= 0; i--){
+            if(nums[i] < nums[i+1]) {
+                ind = i;
+                break;
+            }
         }
-        if(i==-1){
-            reverse(nums.begin(),nums.end());
-            return ;
+        if(ind == -1) {
+            reverse(nums.begin() , nums.end());
+            return;
         }
-        int j = n-1;
-        while(nums[i]>=nums[j]){
-           j--;
+        for(int i = n-1; i > ind ; i--){
+            if(nums[i] > nums[ind]) {
+                swap(nums[i] ,  nums[ind]);
+                break;
+            }
         }
-        swap(nums[i],nums[j]);
-
-        reverse(nums.begin()+i+1,nums.end());
-        return;
-        
-
+        reverse(nums.begin()+ind+1 , nums.end());
+        //[2 , 3 , 5 , 4 , 1] ind = 1 nums[ind] = 2    [2, 4 , 5 , 3 , 1] //[2 , 4 , 1 , 3, 5]
     }
 };
