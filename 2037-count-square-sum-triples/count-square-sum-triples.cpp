23e10a23c0ea5cde11c950e813c693dacb315e99
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2025-12-08 11:08:35 +0530

    Time: 123 ms (24.34%) | Memory: 7.9 MB (21.68%) - LeetSync

---
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countTriples(int n) {
+        int ans =0;
+        for(int i = 1;i<=n ; i++){
+            for(int j = 1 ; j<=n ; j++ ){
+                if(i!=j){
+                    int a = i*i + j*j;
+                    double c  = sqrt(a);
+                    int t = sqrt(a);
+                    if(i*i+j*j == c*c&&c!=i && c!=j&&t==c&&c<=n){
+                        cout<<i <<" "<< j <<" "<<c<<" "<<endl;
+                        ans++;
+                    } 
+                }
+            }
+        }
+        return ans;
+    }
+};
