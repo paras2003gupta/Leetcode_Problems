9d69442aa6f824caf85b5ceda7b26058dee2a8c3
PARAS <116192229+paras2003gupta@users.noreply.github.com>
2026-07-17 16:05:17 +0530

    Time: 0 ms (100%), Space: 23.9 MB (56.45%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int zero_tracker=0,fwd_ptr=0;
-        while(zero_tracker<nums.size()&&nums[zero_tracker]!=0){
-            zero_tracker++;
+        int i = 0;
+        int j = 0;
+        int n = nums.size();
+        while(i<n&&nums[i]!=0){
+            i++;
         }
-        if(zero_tracker==nums.size()){
-            return;
-        }
-        for(int i = zero_tracker+1 ; i < nums.size();i++){
-            if(nums[i]!=0){
-                swap(nums[i],nums[zero_tracker]);
-                while(zero_tracker<nums.size()&&nums[zero_tracker]!=0){
-                    zero_tracker++;
-                }
+        j = i;
+        while(j<n){
+            while(j<n&&nums[j]==0){
+                j++;
             }
-        }
 
-    }
+            if(i<n&&j<n){swap(nums[i],nums[j]);
+                j++;
+                i++;
+            }}
+
+        }
+    
 };
